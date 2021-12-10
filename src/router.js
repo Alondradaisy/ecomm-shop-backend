9dd4edf33f252f525094651cf844d97c7fb45f7b
Alondradaisy <alondra.warren96@gmail.com>
2021-12-10 11:40:04 -0800

    create controller folder to enable user controls

--- src/router.js
+++ src/router.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const ProductService = require("./services/ProductServices");
+
+console.log("ProductService: ", ProductService);
 
 const router = express.Router();
 
-const productServices = require("../src/services/productServices");
+// Product route(s) - Retrieve product data for items \\
+router.get("product-data", ProductService.fetchAllProducts);
+
+// User routes \\
+// login
+// logout
+// register user
+// upload an item to shop
+
+router.post("add-to-bag");
+
+router.delete("remove-from-bag");
 
 module.exports = router;
+
+// --- CRUD requests --- \\
+//Create - post
+//Read/Retrieve - get
+//Update - put
+//Delete - delete
+
+//put hardcoded data in the backend/database
+//have the frontend fetch + display the data
--- src/server.js
+++ src/server.js
@@ -1,27 +1,28 @@
 const express = require("express");
 let cors = require("cors");
 const router = require("./router");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 let cookieParser = require("cookie-parser");
-
+const mongoose = require("mongoose");
 dotenv.config();
 
 const app = express();
 const port = 3020;
 
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-
 //connect to DB with mongoose and copied mongodb link + store in .env
 mongoose
-  .connect(process.env.DB_CONNECTION_STRING)
+  .connect(process.env.DB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MONGO_DB_CONNECTED SUCCESSFULLY"))
-  .then((error) => console.log(error));
+  .catch((error) => console.log(error));
 
 app.use(cors({ credentials: true, origin: "http://localhost:3020" }));
 app.use(cookieParser());
 
+const logger = require("morgan");
+//const bodyParser = require("body-parser");
 //middleware that comes from morgan
 app.use(logger("dev"));
 
@@ -29,8 +30,8 @@ app.use(express.json());
 
 app.use("/", router);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Preview app live at http://localhost:${port}`);
